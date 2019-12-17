aaada1aa65f907e773b775f260d57bec1d0d6fe2
Can Colpan <gitlab@cancolpan.com>
2019-12-17 11:07:37 -0600

    Implementing the Properties for Category, Product, Transaction and User Models

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVALILABLE_PRODUCT = 'available';
+    const UNAVALIABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    public function isAvailable(){
+        return $this->status == Product::AVALILABLE_PRODUCT;
+    }
+
+
 }
