eb3b45f0989c3fae31aa915ce7f341148ede30cd
Can Colpan <gitlab@cancolpan.com>
2019-12-30 10:44:07 -0600

    Implementing The Destroy Method for UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -68,7 +68,7 @@ class UserController extends ApiController
             }
             $user->admin = $request->admin;
         }
-        if (!$user->isDirty()){
+        if (!$user->isDirty()) {
             return response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
         }
 
@@ -79,6 +79,9 @@ class UserController extends ApiController
 
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
     }
 }
