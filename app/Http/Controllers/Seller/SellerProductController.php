05cdbb1fbe329a4632d5bc4d21471c64a2cf9c30
Can Colpan <gitlab@cancolpan.com>
2020-01-07 10:52:54 -0600

    Implementing the Store Method forSeller ProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Seller;
 
-use App\Http\Controllers\ApiController;
+use App\User;
 use App\Seller;
+use App\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
 
 class SellerProductController extends ApiController
 {
@@ -28,9 +30,24 @@ class SellerProductController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, User $seller)
     {
-        //
+        $rules = [
+            'name' =>'required',
+            'description' =>'required',
+            'quantity'=>'required|numeric|min:1',
+            'image'=>'required|image',
+        ];
+        $this->validate($request,$rules);
+
+        $data = $request->all();
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+        return $this->showOne($product);
+
     }
 
    
