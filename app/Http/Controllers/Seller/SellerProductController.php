a10db1ca06bf2cf18173df318d37a342c666d180
Can Colpan <gitlab@cancolpan.com>
2020-01-07 11:16:45 -0600

    Implementing the Update Method for SellerProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Seller;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -33,12 +34,12 @@ class SellerProductController extends ApiController
     public function store(Request $request, User $seller)
     {
         $rules = [
-            'name' =>'required',
-            'description' =>'required',
-            'quantity'=>'required|numeric|min:1',
-            'image'=>'required|image',
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|numeric|min:1',
+            'image' => 'required|image',
         ];
-        $this->validate($request,$rules);
+        $this->validate($request, $rules);
 
         $data = $request->all();
         $data['status'] = Product::UNAVAILABLE_PRODUCT;
@@ -47,10 +48,9 @@ class SellerProductController extends ApiController
 
         $product = Product::create($data);
         return $this->showOne($product);
-
     }
 
-   
+
     /**
      * Update the specified resource in storage.
      *
@@ -58,9 +58,34 @@ class SellerProductController extends ApiController
      * @param  \App\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Seller $seller)
+    public function update(Request $request, Seller $seller, Product $product)
     {
-        //
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' =>  'in:' . Product::AVAILABLE_PRODUCT . ',' . Product::UNAVAILABLE_PRODUCT,
+            'image' => 'image',
+        ];
+        $this->validate($request, $rules);
+        $this->checkSeller($seller, $product);
+
+        $product->fill($request->only([
+            'name',
+            'description',
+            'quantity',
+        ]));
+
+        if ($request->has('status')) {
+            $product->status = $request->status;
+        }
+        if ($product->isAvailable() && $product->categories()->count() == 0) {
+            return $this->errorResponse('An active product must have at least one category', 409);
+        }
+        if ($product->isClean()) {
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+        $product->save();
+
+        return $this->showOne($product);
     }
 
     /**
@@ -72,5 +97,12 @@ class SellerProductController extends ApiController
     public function destroy(Seller $seller)
     {
         //
+    }
+
+    protected function checkSeller(Seller $seller, Product $product)
+    {
+        if ($seller->id != $product->seller_id) {
+            throw new HttpException(422, 'This specified seller is not the actual seller of the product');
+        }
     }
 }
