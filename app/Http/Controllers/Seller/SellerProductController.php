47e5e11327b6288c92b338393de22a49a2ee17a7
Can Colpan <gitlab@cancolpan.com>
2020-01-07 21:24:45 -0600

    Removing the Image When Deleting a Product

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Seller;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
@@ -97,7 +98,11 @@ class SellerProductController extends ApiController
     public function destroy(Seller $seller, Product $product)
     {
         $this->checkSeller($seller, $product);
+
+        Storage::delete($product->image);
+
         $product->delete();
+        
         return $this->showOne($product);
     }
 
