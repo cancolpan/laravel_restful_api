5c0dd8285b17084b37fbc47a9cab69e7d8f14ee2
Can Colpan <gitlab@cancolpan.com>
2020-01-09 10:07:44 -0600

    Send Verification Email Using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Mail\UserCreated;
+use App\User;
 use App\Product;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,6 +31,9 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
+        User::created(function ($user) {
+            Mail::to($user)->send(new UserCreated($user));
+        });
         Product::updated(function ($product) {
             if ($product->quantity == 0 && $product->isAvailable()) {
                 $product->status = Product::UNAVAILABLE_PRODUCT;
