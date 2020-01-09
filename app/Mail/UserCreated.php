9d4e9fc20cd24d81eff99039820aa3baae4cd32a
Can Colpan <gitlab@cancolpan.com>
2020-01-09 09:54:09 -0600

    Implementing the UserCreated Mailable

--- app/Mail/UserCreated.php
+++ app/Mail/UserCreated.php
@@ -10,15 +10,16 @@ use Illuminate\Queue\SerializesModels;
 class UserCreated extends Mailable
 {
     use Queueable, SerializesModels;
-
+    
+    public $user;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -28,6 +29,6 @@ class UserCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->test('email.welcome');
     }
 }
