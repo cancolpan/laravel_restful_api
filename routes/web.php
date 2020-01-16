a8df70e44015c1dc275e76d7d3456a7f7f39a026
Can Colpan <gitlab@cancolpan.com>
2020-01-16 09:56:32 -0600

    Creating the Additional Authentication Structure

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,33 @@
 <?php
 
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+if ($options['register'] ?? true) {
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+}
+
+// Password Reset Routes...
+if ($options['reset'] ?? true) {
+    Route::resetPassword();
+}
+
+// Password Confirmation Routes...
+if (
+    $options['confirm'] ??
+    class_exists(Route::prependGroupNamespace('Auth\ConfirmPasswordController'))
+) {
+    Route::confirmPassword();
+}
+
+// Email Verification Routes...
+if ($options['verify'] ?? false) {
+    Route::emailVerification();
+}
+
+Route::get('/home', 'HomeController@index')->name('home');
